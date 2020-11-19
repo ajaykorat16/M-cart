25234d32d5fe162e8c99a2a15d690caa6ae76e51
Ajay Korat <ajaykorat16@gmail.com>
2020-11-19 17:35:37 +0530

    Finish with API endpoints

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+
 import product from './routes/product.js'
 import user from './routes/user.js'
+import order from './routes/order.js'
+import upload from './routes/upload.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -13,12 +17,29 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Api is running')
-})
-
 app.use('/api/products', product)
 app.use('/api/users', user)
+app.use('/api/orders', order)
+app.use('/api/upload', upload)
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use(notFound)
 
